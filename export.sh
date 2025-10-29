7b0cdf32b702605a31abbc8d102124195fa93e7a
Ivan Žalac <Ivan.Zalac@fer.hr>
2025-10-29 09:08:57 +0100

    fix json

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 mongoexport --db=redditKnjige --collection=knjige_flattened --type=csv --fields=datum,link_reddit,korisnik,broj_upvote,knjiga,ISBN10,autori,područje,potpodručje,link_amazon,ocjena_amazon --out=knjige_ljudi_sa_reddita.csv
-mongoexport --db=redditKnjige --collection=knjige --type=json --out=knjige_ljudi_sa_reddita.json 
+mongoexport --db=redditKnjige --collection=knjige --type=json --jsonArray  --out=knjige_ljudi_sa_reddita.json 
 mongodump --db redditKnjige
 zip -r dump.zip ./dump
 rm -r -v ./dump
